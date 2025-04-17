fc17fd3483c7a127fcacdb2c4533814624f8bdb6
Choerul <achmadchoerul12345@gmail.com>
2025-04-17 23:23:00 +0700

    Belajar Routing dan View

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.beranda');
+});
+Route::get('/about', function() {
+    $biodata = [
+        'nama' => 'Achmad Choerul',
+        'umur' => '20',
+        'alamat' => 'Cilacap'
+    ];
+    return view('pages.about', $biodata);
+});
+
+// menggunakan route view
+Route::view('/contact', 'pages.contact');
+Route::get('/about/{id}/detail', function($id) {
+    return view('pages.detail', [
+        'nomor' => $id
+    ]);
 });
